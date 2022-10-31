99abadab58ddbcdc094c6dc3a0d98f11f74bfc89
sanglijn <88992211+sanglijn@users.noreply.github.com>
2022-10-30 21:03:13 -0400

    Update google.py

--- google.py
+++ google.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID")
 app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
-google_bp = make_google_blueprint(scope=["profile", "email"])
+google_bp = make_google_blueprint(scope=["youtube"])
 app.register_blueprint(google_bp, url_prefix="/login")
 
 @app.route("/")
@@ -15,4 +15,4 @@ def index():
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v1/userinfo")
     assert resp.ok, resp.text
-    return "You are {email} on Google".format(email=resp.json()["email"])
+    return "You are on Google {email} ".format(email=resp.json())
